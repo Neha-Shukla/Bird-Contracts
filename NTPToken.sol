04ad7a6225e2f9ef3ccbfe31748cc4c2b5b6e6cf
NEHA SHUKLA <40213333+Neha-Shukla@users.noreply.github.com>
2021-03-23 22:56:34 +0530

    Update and rename AToken.sol to NTPToken.sol

--- NTPToken.sol
+++ NTPToken.sol
@@ -1,5 +1,4 @@
-
-pragma solidity ^0.6.0;
+pragma solidity >=0.5.15;
 
 interface IERC20 {
 
@@ -17,12 +16,12 @@ interface IERC20 {
 }
 
 
-contract ERC20Basic is IERC20 {
+contract NTP is IERC20 {
 
-    string public constant name = "AToken";
-    string public constant symbol = "A";
-    uint8 public constant decimals = 18;  
-
+    string public constant name = "NTP";
+    string public constant symbol = "NTP";
+    uint8 public constant decimals = 18;
+    uint256 public price;
 
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
     event Transfer(address indexed from, address indexed to, uint tokens);
@@ -31,25 +30,25 @@ contract ERC20Basic is IERC20 {
     mapping(address => uint256) balances;
 
     mapping(address => mapping (address => uint256)) allowed;
-    
-    uint256 totalSupply_ = 10000*(10e18);
+
+    uint256 totalSupply_ = 10000000000000000000000;
 
     using SafeMath for uint256;
 
-   constructor() public {  
-	balances[msg.sender] = totalSupply_;
-    }  
+   constructor() public {
+        balances[msg.sender] = totalSupply_;
+    }
 
     function totalSupply() public override view returns (uint256) {
-	return totalSupply_;
+    return totalSupply_;
     }
-    
+
     function balanceOf(address tokenOwner) public override view returns (uint256) {
         return balances[tokenOwner];
     }
 
     function transfer(address receiver, uint256 numTokens) public override returns (bool) {
-        require(numTokens <= balances[msg.sender], "insufficient");
+        require(numTokens <= balances[msg.sender]);
         balances[msg.sender] = balances[msg.sender].sub(numTokens);
         balances[receiver] = balances[receiver].add(numTokens);
         emit Transfer(msg.sender, receiver, numTokens);
@@ -67,44 +66,83 @@ contract ERC20Basic is IERC20 {
     }
 
     function transferFrom(address owner, address buyer, uint256 numTokens) public override returns (bool) {
-        require(numTokens <= balances[owner],"insufficient");    
-        require(numTokens <= allowed[owner][msg.sender],"not allowed");
-    
+        require(numTokens <= balances[owner]);
+        require(numTokens <= allowed[owner][msg.sender]);
+
         balances[owner] = balances[owner].sub(numTokens);
         allowed[owner][msg.sender] = allowed[owner][msg.sender].sub(numTokens);
         balances[buyer] = balances[buyer].add(numTokens);
         emit Transfer(owner, buyer, numTokens);
         return true;
     }
+        function _mint(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        totalSupply_ = totalSupply_.add(amount);
+        balances[account] = balances[account].add(amount);
+        emit Transfer(address(0), account, amount);
+    }
+
+     /**
+     * @dev Destroys `amount` tokens from `account`, reducing the
+     * total supply.
+     *
+     * Emits a `Transfer` event with `to` set to the zero address.
+     *
+     * Requirements
+     *
+     * - `account` cannot be the zero address.
+     * - `account` must have at least `amount` tokens.
+     */
+    function _burn(address account, uint256 value) internal {
+        require(account != address(0), "ERC20: burn from the zero address");
+
+        totalSupply_ = totalSupply_.sub(value);
+        balances[account] = balances[account].sub(value);
+        emit Transfer(account, address(0), value);
+    }
+    function setPrice(uint256 _price) public{
+        price = _price;
+    }
+    
+    function buy() payable public {
+        _mint(msg.sender,msg.value.div(price));
+    }
+
+    function sell(uint256 amount) public {
+        _burn(msg.sender,amount);
+        msg.sender.transfer(amount.mul(price));
+    }
 }
 
-library SafeMath { 
+library SafeMath {
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
       assert(b <= a);
       return a - b;
     }
-    
+
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
       uint256 c = a + b;
       assert(c >= a);
       return c;
     }
     
-     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        function mul(uint256 a, uint256 b) internal pure returns (uint256) {
         if (a == 0) {
             return 0;
         }
+
         uint256 c = a * b;
-        assert(c / a == b);
+        require(c / a == b);
+
         return c;
     }
 
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0);
         uint256 c = a / b;
+
         return c;
     }
 }
 
-contract AToken is ERC20Basic{
-    
-}
